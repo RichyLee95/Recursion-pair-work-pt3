55ef8a53d30ddbd8c269d9f4cdb8abdf439f3d61
Richard <richylee95@gmail.com>
2023-02-07 16:07:37 -0800

    9 10 11 completed

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -14,8 +14,22 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 
 function flatten(arr) {
   // Your code here
+  let newarr = []
+ for (let i=0;i<arr.length;i++){
+  let el = arr[i]
+  if (Array.isArray(el)){
+    let array = flatten(el)
+    newarr.push(...array)
+  }else{
+    newarr.push(el)
+  }
+
+ }
+ return newarr
 }
-  
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
   
